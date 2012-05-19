acd0ea1143f2975bb0ab9d7643254951d0dca8a3
jakobsulby@gmail.com <jakobsulby@gmail.com@2c6786a7-15cf-2a3c-ba9b-436c5e4e4ad6>
2012-05-19 16:06:21 +0000

    Added navigation to mainView

--- Carpooling/src/dk/iha/carpooling/Main.java
+++ Carpooling/src/dk/iha/carpooling/Main.java
@@ -1,9 +1,13 @@
 package dk.iha.carpooling;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Main extends Activity {
+	
 	/**
 	 * @see android.app.Activity#onCreate(Bundle)
 	 */
@@ -12,5 +16,26 @@ public class Main extends Activity {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.main_view);
+	}
+	
+	public void navigateTo(View view)
+	{
+		switch (view.getId())
+		{
+		case R.id.createRouteBtn:
+			Intent intent = new Intent();
+	        intent.setClass(this, CreateRoute.class);
+	        
+	        startActivityForResult(intent, 1);
+			
+			break;
+
+		default:
+			Toast.makeText(getApplicationContext(), "Method not implemented jet :(", Toast.LENGTH_SHORT).show();
+			
+			break;
+		}
+		
+		
 	}
 }
