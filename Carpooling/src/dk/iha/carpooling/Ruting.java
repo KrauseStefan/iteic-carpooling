843cab26ded681767cd8af1dca8db2a35fa25aa0
jakobsulby@gmail.com <jakobsulby@gmail.com@2c6786a7-15cf-2a3c-ba9b-436c5e4e4ad6>
2012-05-28 09:43:38 +0000

    Changed routing view

--- Carpooling/src/dk/iha/carpooling/Ruting.java
+++ Carpooling/src/dk/iha/carpooling/Ruting.java
@@ -1,11 +1,12 @@
 package dk.iha.carpooling;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
 
-public class Ruting extends MapActivity {
+public class Ruting extends Activity {
 	/**
 	 * @see android.app.Activity#onCreate(Bundle)
 	 */
@@ -13,14 +14,5 @@ public class Ruting extends MapActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.ruting_view);
-		
-		MapView mapView = (MapView) findViewById(R.id.mapview);
-	    mapView.setBuiltInZoomControls(true);
-	}
-
-	@Override
-	protected boolean isRouteDisplayed() {
-		// TODO Auto-generated method stub
-		return false;
 	}
 }
